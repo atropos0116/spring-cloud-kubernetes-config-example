98545d042f68b2af02919327d476335d97ec4813
atropos0116 <37572285+atropos0116@users.noreply.github.com>
2020-04-20 09:53:05 +0900

    first commit
    first commit

---
+++ src/main/java/spring/cloud/kubernetes/config/example/springcloudkubernetesconfigexample/SpringCloudKubernetesConfigExampleApplication.java
@@ -0,0 +1,13 @@
+package spring.cloud.kubernetes.config.example.springcloudkubernetesconfigexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudKubernetesConfigExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudKubernetesConfigExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/cloud/kubernetes/config/example/springcloudkubernetesconfigexample/SpringCloudKubernetesConfigExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.cloud.kubernetes.config.example.springcloudkubernetesconfigexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudKubernetesConfigExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
