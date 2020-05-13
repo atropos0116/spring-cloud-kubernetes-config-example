e521ca06f8e180c896dc1f57d99f6da7095fdeb2
namchae.baik <namchae.baik@bankwareglobal.com>
2020-05-13 14:43:39 +0900

    add skaffold tagger

--- src/main/java/spring/cloud/kubernetes/config/example/springcloudkubernetesconfigexample/WelcomeController.java
+++ src/main/java/spring/cloud/kubernetes/config/example/springcloudkubernetesconfigexample/WelcomeController.java
@@ -1,9 +1,12 @@
 package spring.cloud.kubernetes.config.example.springcloudkubernetesconfigexample;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RequestMapping("/welcome")
 @RestController
@@ -12,11 +15,18 @@ public class WelcomeController {
 	@Value("${message:Welcome to Spring boot}")
 	private String message;
 	
+	@Autowired
+	RestTemplate restTemplate;
+	
 	@GetMapping
 	public String welcome() {
 		System.out.println(message);
 		
-		return message;
+		String url = "http://users-service:8080/users";
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+		System.out.println("Calling via Discovery Client... " + responseEntity.getBody());
+		
+		return message + "\n" + responseEntity.getBody();
 	}
 
 }
